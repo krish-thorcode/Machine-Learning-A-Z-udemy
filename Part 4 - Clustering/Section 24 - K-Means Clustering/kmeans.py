c0c3d07c389a8ebac2b993013fa06d1e4a010d91
krish-thorcode <krish.thorcode@gmail.com>
2018-08-21 09:50:30 +0530

    some issues with plotting the clustering, one cluster is not showing up on the graph..got to leave for stats class..have to do it once i come back

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans.py
@@ -41,3 +41,22 @@ kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10,
 #y_kmeans = kmeans.fit_predict(X); -- this is equivalent to doing the following
 kmeans.fit(X);
 y_kmeans = kmeans.predict(X);
+
+# Visualising the clusters
+plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 60, color = 'red',\
+            label = 'Cluster 1')
+plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 60, color = 'blue',\
+            label = 'Cluster 2')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 60, color = 'green',\
+            label = 'Cluster 3')
+plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 60, color = 'cyan', \
+            label = 'Cluster 4')
+plt.scatter(X[y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 60, color = 'magenta',\
+            label = 'Cluster 5');
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 80,\
+            color = 'yellow', label = 'Centroids')
+plt.title('Cluster of clients')
+plt.xlabel('Annual income (k $)')
+plt.ylabel('Spending score (1-100)')
+plt.legend()
+plt.show()
