af36bb5369d55c8f0d76a79a23ba255433a06cdc
krish-thorcode <krish.thorcode@gmail.com>
2018-08-23 13:30:10 +0530

    implementation done for each review (for loop wrapping around the code segment done)

--- Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/natural_language_processing.py
@@ -10,23 +10,26 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 # Importing the dataset
 df = read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+    
 
-# Cleaning the texts
-review_without_unwanted_characters = re.sub('[^a-zA-Z]', ' ', df['Review'][0])
-review_lowercase = review_without_unwanted_characters.lower()
-
+# Cleaning the reviews and storing them
+corpus = []
 nltk.download('stopwords')
-review_splitted_words = review_lowercase.split()
-# set(stopwords...) is used because casting the words to set() makes the task faster\
-# for the algorithm. without set, ie, stopwords.words('english') would also have worked\
-# but using set() is recommended.
-# review_without_unnecessary_words = [word for word in review_splitted_words\
-#                                    if word not in set(stopwords.words('english'))]
-
-# instead of applying stemming in a separate line, the previous line itself can be\
-# used to stem the words when they are being added the review word list of relevant\
-# words
-ps = PorterStemmer()
-review_stemmed_and_without_unnecessary_words = [ps.stem(word) for word in review_splitted_words\
-                                    if word not in set(stopwords.words('english'))]
-review_cleaned = ' '.join(review_stemmed_and_without_unnecessary_words)
+for i in range(1000): # there are 1000 reviews
+    review_without_unwanted_characters = re.sub('[^a-zA-Z]', ' ', df['Review'][i])
+    review_lowercase = review_without_unwanted_characters.lower()
+    review_splitted_words = review_lowercase.split()
+    # set(stopwords...) is used because casting the words to set() makes the task faster\
+    # for the algorithm. without set, ie, stopwords.words('english') would also have worked\
+    # but using set() is recommended.
+    # review_without_unnecessary_words = [word for word in review_splitted_words\
+    #                                    if word not in set(stopwords.words('english'))]
+    
+    # instead of applying stemming in a separate line, the previous line itself can be\
+    # used to stem the words when they are being added the review word list of relevant\
+    # words
+    ps = PorterStemmer()
+    review_stemmed_and_without_unnecessary_words = [ps.stem(word) for word in review_splitted_words\
+                                        if word not in set(stopwords.words('english'))]
+    review_cleaned = ' '.join(review_stemmed_and_without_unnecessary_words)
+    corpus.append(review_cleaned)
